09fb1b1978bc7bd3a97ee246e8971ee903dc3d91
YOGESH P <yogeshdeepan15@gmail.com>
2025-05-18 19:03:18 +0530

    Create Room reservation.sql

---
+++ Room reservation.sql
@@ -0,0 +1,6 @@
+create database hotel_reservation;
+use hotel_reservation;
+create table booking(first_name varchar(100),second_name varchar(100),contact int8,contact2 int8,address varchar(500),DOB date,email varchar(100),room_type varchar(100),CheckIn_date date,CheckIn_time time,CheckOut_date date);
+select * from booking;
+drop table booking ; 
+
